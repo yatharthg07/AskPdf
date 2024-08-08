a6ceda4c3147afdce9a0fd15efd4d98633fc97bc
Yatharth Gupta <guptayatharth.yg@gmail.com>
2024-08-08 15:43:52 +0530

    Completed pinecode integration of vector pushing

--- src/app/api/create-chat/route.ts
+++ src/app/api/create-chat/route.ts
@@ -1,16 +1,46 @@
+import { db } from "@/lib/db";
+import { chats } from "@/lib/db/schema";
 import { loadS3IntoPinecone } from "@/lib/pinecone";
-import { NextRequest, NextResponse } from "next/server";
+import { getS3Url } from "@/lib/s3";
+import { auth, currentUser } from "@clerk/nextjs/server";
 
-export async function POST(req: Request,res:Response) {
-    try{
-        const body=await req.json();
-        const {file_key,file_name}=body;
-        console.log(file_key,file_name);
-        const pages = await loadS3IntoPinecone(file_key);
-        return NextResponse.json({pages});
-    }
-    catch(e){
-        console.log(e);
-        return NextResponse.json({error:"Internal server error"},{status:500});
-    }
-}
+import { NextResponse } from "next/server";
+
+// /api/create-chat
+export async function POST(req: Request, res: Response) {
+  const { userId } = await auth();
+  if (!userId) {
+    return NextResponse.json({ error: "unauthorized" }, { status: 401 });
+  }
+  try {
+    const body = await req.json();
+    const { file_key, file_name } = body;
+    console.log(file_key, file_name);
+    const docs= await loadS3IntoPinecone(file_key);
+    console.log(docs);
+    const chat_id = await db
+      .insert(chats)
+      .values({
+        fileKey: file_key,
+        pdfName: file_name,
+        pdfUrl: getS3Url(file_key),
+        userId,
+      })
+      .returning({
+        insertedId: chats.id,
+      });
+      // return NextResponse.json({docs});
+    return NextResponse.json(
+      {
+        chat_id: chat_id[0].insertedId,
+      },
+      { status: 200 }
+    );
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json(
+      { error: "internal server error" },
+      { status: 500 }
+    );
+  }
+}   
--- src/lib/s3.ts
+++ src/lib/s3.ts
@@ -38,6 +38,6 @@ export async function uploadToS3(file: File) {
 }
 
 export function getS3Url(file_key: string) {
-    const url = `https://${process.env.NEXT_PUBLIC_S3_BUCKET_NAME}.s3.ap-south-1.amazonaws.com/${file_key}`;
+    const url = `https://${process.env.NEXT_PUBLIC_AWS_BUCKET_NAME}.s3.ap-south-1.amazonaws.com/${file_key}`;
     return url;
   }
