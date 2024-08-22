c1195df565c4efc477e9c06087eedcc3a5549376
Yatharth Gupta <guptayatharth.yg@gmail.com>
2024-08-23 00:56:00 +0530

    made go to chats working

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,9 +4,23 @@ import { auth } from "@clerk/nextjs/server";
 import Link from "next/link";
 import { ArrowRight, LogIn } from "lucide-react";
 import FileUpload from "@/components/FileUpload";
+import { db } from "@/lib/db";
+import { chats } from "@/lib/db/schema";
+import { eq } from "drizzle-orm";
+
+
 export default async function Home() {
   const { userId } = await auth();
   const isAuth = !!userId;
+
+  let firstChat;
+  if (userId) {
+    firstChat = await db.select().from(chats).where(eq(chats.userId, userId));
+    if (firstChat) {
+      firstChat = firstChat[0];
+    }
+  }
+
   return (
     <div className="w-screen min-h-screen bg-gradient-to-r from-indigo-200 via-red-200 to-yellow-100 ">
       <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
@@ -16,10 +30,13 @@ export default async function Home() {
             <UserButton/>
           </div>
           <div className="flex mt-2">
-            {isAuth && (
+            {isAuth && firstChat && (
+
+                <Link href={`/chat/${firstChat.id}`}>
                   <Button>
                     Go to Chats <ArrowRight className="ml-2" />
                   </Button>
+                </Link>
             )}
           </div>
           <p className="max-w-xl mt-1 text-lg text-slate-600">
